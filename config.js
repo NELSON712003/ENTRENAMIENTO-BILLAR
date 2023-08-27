35cc083e7049716700f2d568ac63e5924b06e57b
NELSON712003 <141841863+NELSON712003@users.noreply.github.com>
2023-08-27 10:31:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/985dee976e3b9de3f5f4a19cc7adbd20bc0476adc59a0533491e3fce158eca5d@group.calendar.google.com";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbzKZYghH__ZCL99VhkpXlDZn253OIwX0ZO_8IpGw39Z2xOpOImJu7obgGu_tFqwh24vBA/exec";
